c8451587e2c0c986a8859d66da883a1fc6119a08
Mharika763 <135000543+Mharika763@users.noreply.github.com>
2023-05-30 13:01:40 +0530

    Add files via upload

---
+++ Exp2.java
@@ -0,0 +1,18 @@
+class Exp2
+{
+	int i;
+	public Exp2()
+	{
+	i=10;
+	System.out.println("i="+i);
+}
+	public void display()
+	{
+	System.out.println("welcome");
+	}
+	public static void main(String s[])
+	{
+	Exp2 e=new Exp2();
+	e.display();
+	}
+}
